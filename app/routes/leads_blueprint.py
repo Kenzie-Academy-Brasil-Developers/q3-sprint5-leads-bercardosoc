cea1a912e5bc9ec428a50f1a03d975d5ee6996e4
Bernardo <bernardocardosodacosta@gmail.com>
2022-04-07 03:58:27 -0300

    feat: create routes crud routes and name controller functions

---
+++ app/routes/leads_blueprint.py
@@ -0,0 +1,9 @@
+from flask import Blueprint
+from app.controllers.leads_controller import register_lead, list_leads, update_leads_visits, delete_lead
+
+bp_leads = Blueprint("bp_leads", __name__, url_prefix="/leads")
+
+bp_leads.get("")(list_leads)
+bp_leads.post("")(register_lead)
+bp_leads.patch("<int: id>")(update_leads_visits)
+bp_leads.delete("<int: id>")(delete_lead)
